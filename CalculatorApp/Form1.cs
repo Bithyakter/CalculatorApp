eb7e62a20b4aa01bbed12358ddd7302473323e49
Mst.Bithy Akter <mstbithy@outlook.com>
2024-02-02 16:55:32 +0600

    update code pattern

--- CalculatorApp/Form1.cs
+++ CalculatorApp/Form1.cs
@@ -12,7 +12,6 @@ namespace CalculatorApp
 
       }
 
-      string CalTotal;
       int num1;
       int num2;
       string operation;
@@ -21,52 +20,6 @@ namespace CalculatorApp
       private void textBox1_TextChanged(object sender, EventArgs e)
       {
 
-      }
-
-      private void button1_Click(object sender, EventArgs e)
-      {
-         txtDisplay.Text = txtDisplay.Text + "7";
-      }
-
-      private void button4_Click(object sender, EventArgs e)
-      {
-         txtDisplay.Text = txtDisplay.Text + "8";
-      }
-
-      private void button3_Click(object sender, EventArgs e)
-      {
-         txtDisplay.Text = txtDisplay.Text + "9";
-      }
-
-      private void button2_Click(object sender, EventArgs e)
-      {
-         operation = "/";
-         num1 = int.Parse(txtDisplay.Text);
-
-         txtDisplay.Clear();
-      }
-
-      private void button1_Click_1(object sender, EventArgs e)
-      {
-         txtDisplay.Text = txtDisplay.Text + "4";
-      }
-
-      private void button5_Click(object sender, EventArgs e)
-      {
-         txtDisplay.Text = txtDisplay.Text + "5";
-      }
-
-      private void button6_Click(object sender, EventArgs e)
-      {
-         txtDisplay.Text = txtDisplay.Text + "6";
-      }
-
-      private void button7_Click(object sender, EventArgs e)
-      {
-         operation = "*";
-         num1 = int.Parse(txtDisplay.Text);
-
-         txtDisplay.Clear();
       }
 
       private void button8_Click(object sender, EventArgs e)
@@ -84,6 +37,49 @@ namespace CalculatorApp
          txtDisplay.Text = txtDisplay.Text + "3";
       }
 
+      private void button1_Click_1(object sender, EventArgs e)
+      {
+         txtDisplay.Text = txtDisplay.Text + "4";
+      }
+
+      private void button5_Click(object sender, EventArgs e)
+      {
+         txtDisplay.Text = txtDisplay.Text + "5";
+      }
+
+      private void button6_Click(object sender, EventArgs e)
+      {
+         txtDisplay.Text = txtDisplay.Text + "6";
+      }
+
+      private void button1_Click(object sender, EventArgs e)
+      {
+         txtDisplay.Text = txtDisplay.Text + "7";
+      }
+
+      private void button4_Click(object sender, EventArgs e)
+      {
+         txtDisplay.Text = txtDisplay.Text + "8";
+      }
+
+      private void button3_Click(object sender, EventArgs e)
+      {
+         txtDisplay.Text = txtDisplay.Text + "9";
+      }
+
+      private void button13_Click(object sender, EventArgs e)
+      {
+         txtDisplay.Text = txtDisplay.Text + "0";
+      }
+
+      private void button15_Click(object sender, EventArgs e)
+      {
+         operation = "+";
+         num1 = int.Parse(txtDisplay.Text);
+
+         txtDisplay.Clear();
+      }
+
       private void button11_Click(object sender, EventArgs e)
       {
          operation = "-";
@@ -92,14 +88,19 @@ namespace CalculatorApp
          txtDisplay.Clear();
       }
 
-      private void button12_Click(object sender, EventArgs e)
+      private void button7_Click(object sender, EventArgs e)
       {
+         operation = "*";
+         num1 = int.Parse(txtDisplay.Text);
+
          txtDisplay.Clear();
       }
-
-      private void button13_Click(object sender, EventArgs e)
+      private void button2_Click(object sender, EventArgs e)
       {
-         txtDisplay.Text = txtDisplay.Text + "0";
+         operation = "/";
+         num1 = int.Parse(txtDisplay.Text);
+
+         txtDisplay.Clear();
       }
 
       private void button14_Click(object sender, EventArgs e)
@@ -121,11 +122,8 @@ namespace CalculatorApp
          txtDisplay.Text = result + "";
       }
 
-      private void button15_Click(object sender, EventArgs e)
+      private void button12_Click(object sender, EventArgs e)
       {
-         operation = "+";
-         num1 = int.Parse(txtDisplay.Text);
-
          txtDisplay.Clear();
       }
    }
